2dddaf7f69bdbe0ca49c6aa42f85c4b2c3797c95
denis <denbro228822@gmail.com>
2026-04-20 22:21:35 +0300

    fix vendor

--- vendor/esp-alloc/src/heap/mod.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(heap_algorithm_llff)]
-mod llff;
-#[cfg(heap_algorithm_tlsf)]
-mod tlsf;
-
-#[cfg(heap_algorithm_llff)]
-pub(crate) use llff::LlffHeap as Heap;
-#[cfg(heap_algorithm_tlsf)]
-pub(crate) use tlsf::TlsfHeap as Heap;
